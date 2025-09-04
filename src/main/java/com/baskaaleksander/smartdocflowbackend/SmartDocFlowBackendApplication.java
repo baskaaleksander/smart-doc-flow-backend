4335738dcfb82a9068b5eb23eccd326b4718e028
Aleksander Baska <baskaaleksander03@gmail.com>
2025-09-04 18:41:14 +0200

    initial commit

---
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/SmartDocFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.baskaaleksander.smartdocflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDocFlowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartDocFlowBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baskaaleksander/smartdocflowbackend/SmartDocFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baskaaleksander.smartdocflowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDocFlowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
