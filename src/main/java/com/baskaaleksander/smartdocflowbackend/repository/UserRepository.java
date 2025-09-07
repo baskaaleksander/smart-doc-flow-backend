cf2055d89b11e69467a81cdf5d202467fb907e18
Aleksander Baska <baskaaleksander03@gmail.com>
2025-09-07 08:19:28 +0200

    feat: modified user details service to retrieve user roles

--- src/main/java/com/baskaaleksander/smartdocflowbackend/repository/UserRepository.java
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/repository/UserRepository.java
@@ -2,10 +2,15 @@ package com.baskaaleksander.smartdocflowbackend.repository;
 
 import com.baskaaleksander.smartdocflowbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+
+    @Query("select u from User u left join fetch u.roles where u.username = :username")
+    Optional<User> findUserByUsernameWithRoles(String username);
+
 }
--- src/main/java/com/baskaaleksander/smartdocflowbackend/service/CustomUserDetailsService.java
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/service/CustomUserDetailsService.java
@@ -4,12 +4,13 @@ import com.baskaaleksander.smartdocflowbackend.exceptions.ResourceNotFoundExcept
 import com.baskaaleksander.smartdocflowbackend.model.User;
 import com.baskaaleksander.smartdocflowbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.List;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -23,13 +24,17 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
+        User user = userRepository.findUserByUsernameWithRoles(username)
                 .orElseThrow(() -> new ResourceNotFoundException("User with username " + username + " not found"));
 
+        List<SimpleGrantedAuthority> authorities = user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRole()))
+                .toList();
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
                 user.getPassword(),
-                Collections.emptyList()
+                authorities
         );
     }
 }
