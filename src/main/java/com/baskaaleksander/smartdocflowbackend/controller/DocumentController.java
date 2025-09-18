6b760a428616fee91fad5276480d1f9e4173ecff
Aleksander Baska <baskaaleksander03@gmail.com>
2025-09-18 23:32:15 +0200

    feat: rename notification endpoint to /notifications for consistency

--- src/main/java/com/baskaaleksander/smartdocflowbackend/controller/DocumentController.java
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/controller/DocumentController.java
@@ -9,13 +9,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
--- src/main/java/com/baskaaleksander/smartdocflowbackend/controller/NotificationController.java
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/controller/NotificationController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/notification")
+@RequestMapping("/notifications")
 public class NotificationController {
 
     private final NotificationService notificationService;
