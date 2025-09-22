7eb65a080abf39891c8986ba57e0ed007ac50a57
Aleksander Baska <baskaaleksander03@gmail.com>
2025-09-22 17:30:51 +0200

    feat: add VectorStoreLoader service for loading document chunks into vector store

---
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/modules/documents/application/impl/VectorStoreLoader.java
@@ -0,0 +1,33 @@
+package com.baskaaleksander.smartdocflowbackend.modules.documents.application.impl;
+
+import com.baskaaleksander.smartdocflowbackend.modules.documents.domain.Chunk;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class VectorStoreLoader {
+
+    private final VectorStore vectorStore;
+
+    public void loadChunks(List<Chunk> chunks) {
+        List<Document> documents = chunks.stream().map(
+                c -> Document.builder()
+                        .text(c.content())
+                        .metadata(Map.of(
+                                "docId", c.documentId(),
+                                "page", c.page(),
+                                "startOffset", c.startOffset(),
+                                "endOffset", c.endOffset()
+                        ))
+                        .build()
+        ).toList();
+
+        vectorStore.add(documents);
+    }
+}
