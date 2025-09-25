7d7e584c1228b1729d9438c43cfa2b059b239504
Aleksander Baska <baskaaleksander03@gmail.com>
2025-09-25 18:12:44 +0200

    feat: add EmbedTaskListener and EmbedTaskPublisher for embedding tasks management

---
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/modules/documents/application/impl/EmbedTaskListener.java
@@ -0,0 +1,7 @@
+package com.baskaaleksander.smartdocflowbackend.modules.documents.application.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmbedTaskListener {
+}
---
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/modules/documents/application/impl/EmbedTaskPublisher.java
@@ -0,0 +1,27 @@
+package com.baskaaleksander.smartdocflowbackend.modules.documents.application.impl;
+
+import com.baskaaleksander.smartdocflowbackend.common.config.QueueConfig;
+import com.baskaaleksander.smartdocflowbackend.modules.documents.domain.EmbedTask;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmbedTaskPublisher {
+    private final RabbitTemplate rabbitTemplate;
+
+    public EmbedTaskPublisher(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void enqueue(UUID documentId) {
+        EmbedTask task = new EmbedTask(documentId);
+
+        rabbitTemplate.convertAndSend(
+                QueueConfig.EXCHANGE,
+                QueueConfig.EMBED_ROUTING_KEY,
+                task
+        );
+    }
+}
