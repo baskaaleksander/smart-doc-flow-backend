9a51629f9e1186c916546674573b8ba8bd6b9cac
Aleksander Baska <baskaaleksander03@gmail.com>
2025-09-29 15:26:40 +0200

    feat: add query method to retrieve userId by ConversationMessage ID in ConversationMessageRepository

--- src/main/java/com/baskaaleksander/smartdocflowbackend/modules/documents/persistence/ConversationMessageRepository.java
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/modules/documents/persistence/ConversationMessageRepository.java
@@ -1,10 +1,15 @@
 package com.baskaaleksander.smartdocflowbackend.modules.documents.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface ConversationMessageRepository extends JpaRepository<ConversationMessage, UUID> {
+
+    @Query("select c.userId from ConversationMessage c where c.id = :id")
+    UUID getUserIdById(UUID id);
+
 }
