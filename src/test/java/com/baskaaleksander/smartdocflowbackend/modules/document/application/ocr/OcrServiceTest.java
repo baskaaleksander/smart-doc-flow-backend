c55bc909a286525ce54ba9819b7ab5f708b254fa
Aleksander Baska <baskaaleksander03@gmail.com>
2025-10-05 17:23:52 +0200

    test: add unit tests for DocumentService and OcrService to verify functionality

---
+++ src/test/java/com/baskaaleksander/smartdocflowbackend/modules/document/application/ocr/OcrServiceTest.java
@@ -0,0 +1,8 @@
+package com.baskaaleksander.smartdocflowbackend.modules.document.application.ocr;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class OcrServiceTest {
+}
