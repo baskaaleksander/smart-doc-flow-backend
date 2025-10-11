5d857b39cceef311ac9942d08843638f28f84ff4
Aleksander Baska <baskaaleksander03@gmail.com>
2025-10-11 11:46:42 +0200

    feat: enhance DocumentMapper to include basic info for document owner and review

--- src/main/java/com/baskaaleksander/smartdocflowbackend/modules/documents/mapping/DocumentMapper.java
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/modules/documents/mapping/DocumentMapper.java
@@ -1,8 +1,11 @@
 package com.baskaaleksander.smartdocflowbackend.modules.documents.mapping;
 
+import com.baskaaleksander.smartdocflowbackend.modules.documents.api.dto.DocumentOwnerBasicInfo;
 import com.baskaaleksander.smartdocflowbackend.modules.documents.api.dto.DocumentResponse;
+import com.baskaaleksander.smartdocflowbackend.modules.documents.api.dto.DocumentReviewBasicInfo;
 import com.baskaaleksander.smartdocflowbackend.modules.documents.persistence.Document;
 import com.baskaaleksander.smartdocflowbackend.modules.reviews.persistence.Review;
+import com.baskaaleksander.smartdocflowbackend.modules.users.persistence.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
@@ -17,13 +20,29 @@ public interface DocumentMapper {
     DocumentMapper INSTANCE = Mappers.getMapper(DocumentMapper.class);
 
     @Mappings({
-            @Mapping(target = "ownerId", source = "owner.id"),
-            @Mapping(target = "reviewId", source = "review", qualifiedByName = "reviewToId"),
+            @Mapping(target = "owner", source = "owner", qualifiedByName = "ownerToBasicInfo"),
+            @Mapping(target = "review", source = "review", qualifiedByName = "reviewToBasicInfo"),
     })
     DocumentResponse toDocumentResponse(Document document);
 
-    @Named("reviewToId")
-    default UUID reviewToId(Review review) {
-        return review == null ? null : review.getId();
+    @Named("reviewToBasicInfo")
+    default DocumentReviewBasicInfo reviewToBasicInfo(Review review) {
+        return review == null ? null :
+                new DocumentReviewBasicInfo(
+                        review.getId(),
+                        review.getReviewer().getUsername(),
+                        review.getReviewer().getId(),
+                        review.getStatus(),
+                        review.getUpdatedAt()
+                );
+    }
+
+    @Named("ownerToBasicInfo")
+    default DocumentOwnerBasicInfo ownerToBasicInfo(User owner) {
+        return owner == null ? null :
+                new DocumentOwnerBasicInfo(
+                        owner.getId(),
+                        owner.getUsername()
+                );
     }
 }
