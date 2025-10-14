6c76606ee9fbf81e457b4e5317de46678d1ff637
Aleksander Baska <baskaaleksander03@gmail.com>
2025-10-14 17:38:48 +0200

    feat: add NotificationEventListener to handle notification events

---
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/modules/notifications/application/NotificationEventListener.java
@@ -0,0 +1,22 @@
+package com.baskaaleksander.smartdocflowbackend.modules.notifications.application;
+
+import com.baskaaleksander.smartdocflowbackend.modules.notifications.domain.NotificationEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationEventListener {
+
+    private final NotificationService notificationService;
+
+    public NotificationEventListener(
+            NotificationService notificationService
+    ) {
+        this.notificationService = notificationService;
+    }
+
+    @EventListener
+    public void handleNotificationEvent(NotificationEvent event) {
+        notificationService.sendNotification(event.username(), event.type(), event.message());
+    }
+}
