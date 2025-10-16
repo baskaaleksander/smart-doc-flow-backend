df31609a880a40aa826c3cf9acf9c8194268f217
Aleksander Baska <baskaaleksander03@gmail.com>
2025-10-16 21:13:33 +0200

    feat: add TokenGenerator class for secure token generation

---
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/common/util/TokenGenerator.java
@@ -0,0 +1,15 @@
+package com.baskaaleksander.smartdocflowbackend.common.util;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class TokenGenerator {
+    private static final SecureRandom random = new SecureRandom();
+
+    public static String generateToken() {
+        byte[] bytes = new byte[32];
+        random.nextBytes(bytes);
+
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
+    }
+}
