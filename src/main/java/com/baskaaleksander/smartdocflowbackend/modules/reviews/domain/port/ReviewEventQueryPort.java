68cb3421eab77d61ad25628b3a743323a1fb822c
Aleksander Baska <baskaaleksander03@gmail.com>
2025-10-18 11:52:40 +0200

    feat: add ReviewEventCommandPort and ReviewEventQueryPort interfaces for review management

---
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/modules/reviews/domain/port/ReviewEventQueryPort.java
@@ -0,0 +1,4 @@
+package com.baskaaleksander.smartdocflowbackend.modules.reviews.domain.port;
+
+public interface ReviewEventQueryPort {
+}
---
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/modules/reviews/domain/port/ReviewQueryPort.java
@@ -0,0 +1,15 @@
+package com.baskaaleksander.smartdocflowbackend.modules.reviews.domain.port;
+
+import com.baskaaleksander.smartdocflowbackend.common.pagination.PaginationRequest;
+import com.baskaaleksander.smartdocflowbackend.common.pagination.PagingResult;
+import com.baskaaleksander.smartdocflowbackend.modules.reviews.domain.model.Review;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ReviewQueryPort {
+    Optional<UUID> getReviewerIdByDocumentId(UUID documentId);
+    Optional<Review> getReviewById(UUID id);
+    PagingResult<Review> findAll(PaginationRequest request);
+    PagingResult<Review> findByStatus(PaginationRequest request);
+}
