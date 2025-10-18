adaa117c961c919c12a3c1733aa63106c930a76f
Aleksander Baska <baskaaleksander03@gmail.com>
2025-10-18 19:35:00 +0200

    feat: implement pagination for notification retrieval and update mark read methods

--- src/main/java/com/baskaaleksander/smartdocflowbackend/modules/notifications/adapters/persistence/NotificationJpaAdapter.java
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/modules/notifications/adapters/persistence/NotificationJpaAdapter.java
@@ -1,14 +1,19 @@
 package com.baskaaleksander.smartdocflowbackend.modules.notifications.adapters.persistence;
 
 import com.baskaaleksander.smartdocflowbackend.common.pagination.PaginationRequest;
+import com.baskaaleksander.smartdocflowbackend.common.pagination.PaginationUtil;
 import com.baskaaleksander.smartdocflowbackend.common.pagination.PagingResult;
+import com.baskaaleksander.smartdocflowbackend.modules.notifications.adapters.persistence.entity.NotificationEntity;
 import com.baskaaleksander.smartdocflowbackend.modules.notifications.adapters.persistence.spring.SpringDataNotificationRepository;
 import com.baskaaleksander.smartdocflowbackend.modules.notifications.domain.model.Notification;
 import com.baskaaleksander.smartdocflowbackend.modules.notifications.domain.port.NotificationCommandPort;
 import com.baskaaleksander.smartdocflowbackend.modules.notifications.domain.port.NotificationQueryPort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -31,26 +36,52 @@ public class NotificationJpaAdapter implements NotificationCommandPort, Notifica
 
     @Override
     public Integer markOneRead(String username, UUID id) {
-        return 0;
+        return notificationRepo.markOneRead(username, id);
     }
 
     @Override
     public Integer markAllRead(String username) {
-        return 0;
+        return notificationRepo.markAllRead(username);
     }
 
     @Override
     public PagingResult<Notification> findAllByUsernameAndRead(PaginationRequest request, String username, boolean read) {
-        return null;
+        Pageable pageable = PaginationUtil.getPageable(request);
+        Page<NotificationEntity> entities = notificationRepo.findAllByUsernameAndRead(pageable, username, read);
+
+        List<Notification> content = entities.getContent().stream().map(mapper::toDomain).toList();
+
+        return new PagingResult<>(
+                content,
+                entities.getTotalPages(),
+                entities.getTotalElements(),
+                entities.getSize(),
+                entities.getNumber(),
+                entities.isLast(),
+                entities.hasNext()
+        );
     }
 
     @Override
     public PagingResult<Notification> findAllByUsername(PaginationRequest request, String username) {
-        return null;
+        Pageable pageable = PaginationUtil.getPageable(request);
+        Page<NotificationEntity> entities = notificationRepo.findAllByUsername(pageable, username);
+
+        List<Notification> content = entities.getContent().stream().map(mapper::toDomain).toList();
+
+        return new PagingResult<>(
+                content,
+                entities.getTotalPages(),
+                entities.getTotalElements(),
+                entities.getSize(),
+                entities.getNumber(),
+                entities.isLast(),
+                entities.hasNext()
+        );
     }
 
     @Override
     public Integer getNotificationsCountByUsernameAndRead(String username, boolean read) {
-        return 0;
+        return notificationRepo.getNotificationsCountByUsernameAndRead(username, read);
     }
 }
