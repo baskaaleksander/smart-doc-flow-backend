4ae96d86ca742d81975c0c3251bfe1a2e6f9f008
Aleksander Baska <baskaaleksander03@gmail.com>
2025-10-19 12:56:26 +0200

    feat: implement save method in PasswordResetTokenJpaAdapter and enhance token retrieval functionality

--- src/main/java/com/baskaaleksander/smartdocflowbackend/modules/auth/adapters/persistence/PasswordResetTokenJpaAdapter.java
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/modules/auth/adapters/persistence/PasswordResetTokenJpaAdapter.java
@@ -1,8 +1,12 @@
 package com.baskaaleksander.smartdocflowbackend.modules.auth.adapters.persistence;
 
+import com.baskaaleksander.smartdocflowbackend.modules.auth.adapters.persistence.entity.PasswordResetTokenEntity;
+import com.baskaaleksander.smartdocflowbackend.modules.auth.adapters.persistence.mapping.PasswordResetTokenPersistenceMapper;
+import com.baskaaleksander.smartdocflowbackend.modules.auth.adapters.persistence.spring.SpringDataPasswordResetTokenRepository;
 import com.baskaaleksander.smartdocflowbackend.modules.auth.domain.model.PasswordResetToken;
 import com.baskaaleksander.smartdocflowbackend.modules.auth.domain.port.PasswordResetTokenCommandPort;
 import com.baskaaleksander.smartdocflowbackend.modules.auth.domain.port.PasswordResetTokenQueryPort;
+import com.baskaaleksander.smartdocflowbackend.modules.users.adapters.persistence.spring.SpringDataUserRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,18 +16,48 @@ import java.util.UUID;
 @Repository
 @Transactional(readOnly = true)
 public class PasswordResetTokenJpaAdapter implements PasswordResetTokenCommandPort, PasswordResetTokenQueryPort {
+
+    private final SpringDataPasswordResetTokenRepository tokenRepo;
+    private final SpringDataUserRepository userRepo;
+    private final PasswordResetTokenPersistenceMapper mapper;
+
+    public PasswordResetTokenJpaAdapter(
+            SpringDataPasswordResetTokenRepository tokenRepo,
+            SpringDataUserRepository userRepo,
+            PasswordResetTokenPersistenceMapper mapper
+    ) {
+        this.tokenRepo = tokenRepo;
+        this.userRepo = userRepo;
+        this.mapper = mapper;
+    }
     @Override
     public PasswordResetToken save(PasswordResetToken token) {
-        return null;
+        PasswordResetTokenEntity entity = (token.getId() != null) ?
+                tokenRepo.findById(token.getId()).orElseGet(() -> {
+                    PasswordResetTokenEntity e = new PasswordResetTokenEntity();
+                    e.setId(token.getId());
+                    return e;
+                })
+                : new PasswordResetTokenEntity();
+
+        entity.setToken(token.getToken());
+        entity.setExpiresAt(token.getExpiresAt());
+        entity.setRevoked(token.isRevoked());
+
+        entity.setUser(userRepo.getReferenceById(token.getUserId()));
+
+        PasswordResetTokenEntity saved = tokenRepo.save(entity);
+
+        return mapper.toDomain(saved);
     }
 
     @Override
     public Integer invalidateAllTokens(UUID userId) {
-        return 0;
+        return tokenRepo.invalidateAllTokens(userId);
     }
 
     @Override
     public Optional<PasswordResetToken> findByToken(String token) {
-        return Optional.empty();
+        return tokenRepo.findByToken(token).map(mapper::toDomain);
     }
 }
