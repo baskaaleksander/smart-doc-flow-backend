173b9e823fce5cbf2cc091e3c450e4f54edd322a
Aleksander Baska <baskaaleksander03@gmail.com>
2025-10-20 17:55:35 +0200

    feat: add save method to DocumentOcrResultCommandPort and getOcrByDocId method to DocumentOcrResultQueryPort

--- src/main/java/com/baskaaleksander/smartdocflowbackend/modules/documents/domain/port/DocumentOcrResultCommandPort.java
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/modules/documents/domain/port/DocumentOcrResultCommandPort.java
@@ -1,4 +1,7 @@
 package com.baskaaleksander.smartdocflowbackend.modules.documents.domain.port;
 
+import com.baskaaleksander.smartdocflowbackend.modules.documents.domain.model.DocumentOcrResult;
+
 public interface DocumentOcrResultCommandPort {
+    DocumentOcrResult save(DocumentOcrResult result);
 }
--- src/main/java/com/baskaaleksander/smartdocflowbackend/modules/documents/domain/port/DocumentOcrResultQueryPort.java
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/modules/documents/domain/port/DocumentOcrResultQueryPort.java
@@ -1,4 +1,10 @@
 package com.baskaaleksander.smartdocflowbackend.modules.documents.domain.port;
 
+import com.baskaaleksander.smartdocflowbackend.modules.documents.domain.model.DocumentOcrResult;
+
+import java.util.Optional;
+import java.util.UUID;
+
 public interface DocumentOcrResultQueryPort {
+    Optional<DocumentOcrResult> getOcrByDocId(UUID documentId);
 }
