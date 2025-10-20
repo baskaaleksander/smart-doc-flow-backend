0c694c093c42fd4402be4bf4156bcbcdc413e993
Aleksander Baska <baskaaleksander03@gmail.com>
2025-10-20 18:13:34 +0200

    feat: enhance ConversationMessageJpaAdapter with save, delete, and retrieval methods; integrate JdbcChatMemoryRepository for conversation management

--- src/main/java/com/baskaaleksander/smartdocflowbackend/modules/documents/adapters/persistence/jpa/ConversationMessageJpaAdapter.java
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/modules/documents/adapters/persistence/jpa/ConversationMessageJpaAdapter.java
@@ -1,10 +1,17 @@
 package com.baskaaleksander.smartdocflowbackend.modules.documents.adapters.persistence.jpa;
 
 import com.baskaaleksander.smartdocflowbackend.common.pagination.PaginationRequest;
+import com.baskaaleksander.smartdocflowbackend.common.pagination.PaginationUtil;
 import com.baskaaleksander.smartdocflowbackend.common.pagination.PagingResult;
+import com.baskaaleksander.smartdocflowbackend.modules.documents.adapters.persistence.entity.ConversationMessageEntity;
+import com.baskaaleksander.smartdocflowbackend.modules.documents.adapters.persistence.mapping.ConversationMessagePersistenceMapper;
+import com.baskaaleksander.smartdocflowbackend.modules.documents.adapters.persistence.spring.SpringDataConversationMessageRepository;
 import com.baskaaleksander.smartdocflowbackend.modules.documents.domain.model.ConversationMessage;
 import com.baskaaleksander.smartdocflowbackend.modules.documents.domain.port.ConversationMessageCommandPort;
 import com.baskaaleksander.smartdocflowbackend.modules.documents.domain.port.ConversationMessageQueryPort;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,30 +22,72 @@ import java.util.UUID;
 @Repository
 @Transactional(readOnly = true)
 public class ConversationMessageJpaAdapter implements ConversationMessageCommandPort, ConversationMessageQueryPort {
+
+    private final SpringDataConversationMessageRepository conversationRepo;
+    private final JdbcChatMemoryRepository jdbcChatMemoryRepository;
+    private final ConversationMessagePersistenceMapper mapper;
+
+    public ConversationMessageJpaAdapter(
+            SpringDataConversationMessageRepository conversationRepo,
+            JdbcChatMemoryRepository jdbcChatMemoryRepository,
+            ConversationMessagePersistenceMapper mapper
+            ) {
+        this.conversationRepo = conversationRepo;
+        this.jdbcChatMemoryRepository = jdbcChatMemoryRepository;
+        this.mapper = mapper;
+    }
+
     @Override
     @Transactional
     public ConversationMessage save(ConversationMessage message) {
-        return null;
+        ConversationMessageEntity entity = (message.getId() != null) ?
+                conversationRepo.findById(message.getId()).orElseGet(ConversationMessageEntity::new)
+                : new ConversationMessageEntity();
+
+        entity.setSide(message.getSide());
+        entity.setContent(message.getContent());
+        entity.setFingerprint(message.getFingerprint());
+        entity.setConversationId(message.getConversationId());
+        entity.setDocumentId(message.getDocumentId());
+        entity.setUserId(message.getUserId());
+
+        ConversationMessageEntity saved = conversationRepo.save(entity);
+
+        return mapper.toDomain(saved);
     }
 
     @Override
     @Transactional
     public void deleteAllByConversationId(UUID conversationId) {
-
+        conversationRepo.deleteAllByConversationId(conversationId);
+        jdbcChatMemoryRepository.deleteByConversationId(conversationId.toString());
     }
 
     @Override
     public List<UUID> getUserIdByDocumentId(UUID documentId) {
-        return List.of();
+        return conversationRepo.getUserIdByDocumentId(documentId);
     }
 
     @Override
     public Optional<UUID> getIdByUserIdAndDocId(UUID userId, UUID documentId) {
-        return Optional.empty();
+        return conversationRepo.getIdByUserIdAndDocId(userId, documentId);
     }
 
     @Override
     public PagingResult<ConversationMessage> findAllByDocumentIdAndUserId(PaginationRequest request, UUID userId, UUID documentId) {
-        return null;
+        Pageable pageable = PaginationUtil.getPageable(request);
+
+        Page<ConversationMessageEntity> entities = conversationRepo.findAllByDocumentIdAndUserId(pageable, userId, documentId);
+        List<ConversationMessage> content = entities.getContent().stream().map(mapper::toDomain).toList();
+
+        return new PagingResult<>(
+                content,
+                entities.getTotalPages(),
+                entities.getTotalElements(),
+                entities.getSize(),
+                entities.getNumber(),
+                entities.isLast(),
+                entities.hasNext()
+        );
     }
 }
