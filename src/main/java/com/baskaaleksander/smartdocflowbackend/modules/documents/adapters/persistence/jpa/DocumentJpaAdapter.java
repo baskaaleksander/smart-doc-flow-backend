2d907416901428a44ce12635733672e095601c67
Aleksander Baska <baskaaleksander03@gmail.com>
2025-10-23 15:12:54 +0200

    feat: implement DocumentJpaAdapter methods for document retrieval and pagination

--- src/main/java/com/baskaaleksander/smartdocflowbackend/modules/documents/adapters/persistence/jpa/DocumentJpaAdapter.java
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/modules/documents/adapters/persistence/jpa/DocumentJpaAdapter.java
@@ -1,13 +1,19 @@
 package com.baskaaleksander.smartdocflowbackend.modules.documents.adapters.persistence.jpa;
 
 import com.baskaaleksander.smartdocflowbackend.common.pagination.PaginationRequest;
+import com.baskaaleksander.smartdocflowbackend.common.pagination.PaginationUtil;
 import com.baskaaleksander.smartdocflowbackend.common.pagination.PagingResult;
+import com.baskaaleksander.smartdocflowbackend.modules.documents.adapters.persistence.entity.DocumentEntity;
+import com.baskaaleksander.smartdocflowbackend.modules.documents.adapters.persistence.mapping.DocumentPersistenceMapper;
+import com.baskaaleksander.smartdocflowbackend.modules.documents.adapters.persistence.spring.SpringDataDocumentRepository;
 import com.baskaaleksander.smartdocflowbackend.modules.documents.domain.model.Document;
 import com.baskaaleksander.smartdocflowbackend.modules.documents.domain.model.DocumentReviewBasic;
 import com.baskaaleksander.smartdocflowbackend.modules.documents.domain.model.DocumentStatus;
 import com.baskaaleksander.smartdocflowbackend.modules.documents.domain.port.DocumentCommandPort;
 import com.baskaaleksander.smartdocflowbackend.modules.documents.domain.port.DocumentQueryPort;
 import com.baskaaleksander.smartdocflowbackend.modules.documents.domain.view.DocumentStatusCount;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,10 +21,23 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Repository
 @Transactional(readOnly = true)
 public class DocumentJpaAdapter implements DocumentCommandPort, DocumentQueryPort {
+
+    private final SpringDataDocumentRepository documentRepo;
+    private final DocumentPersistenceMapper mapper;
+
+    public DocumentJpaAdapter(
+            SpringDataDocumentRepository documentRepo,
+            DocumentPersistenceMapper mapper
+    ) {
+        this.documentRepo = documentRepo;
+        this.mapper = mapper;
+    }
+
     @Override
     public Document save(Document document) {
         return null;
@@ -36,41 +55,77 @@ public class DocumentJpaAdapter implements DocumentCommandPort, DocumentQueryPor
 
     @Override
     public Optional<Document> getDocumentById(UUID documentId) {
-        return Optional.empty();
+        return documentRepo.getDocumentById(documentId).map(mapper::toDomain);
     }
 
     @Override
     public Optional<String> getOwnerUsernameById(UUID documentId) {
-        return Optional.empty();
+        return documentRepo.getOwnerUsernameById(documentId);
     }
 
     @Override
     public Optional<Document> findByIdWithReview(UUID documentId) {
-        return Optional.empty();
+        return documentRepo.findbyIdWithReview(documentId).map(mapper::toDomain);
     }
 
     @Override
     public PagingResult<Document> findAllByOwner(UUID ownerId, PaginationRequest request) {
-        return null;
+        Pageable pageable = PaginationUtil.getPageable(request);
+        Page<DocumentEntity> entities = documentRepo.findAllByOwner(ownerId ,pageable);
+        List<Document> content = entities.getContent().stream().map(mapper::toDomain).toList();
+
+        return new PagingResult<>(
+                content,
+                entities.getTotalPages(),
+                entities.getTotalElements(),
+                entities.getSize(),
+                entities.getNumber(),
+                entities.isLast(),
+                entities.hasNext()
+        );
     }
 
     @Override
     public PagingResult<Document> findAllByReviewer(UUID reviewerId, PaginationRequest request) {
-        return null;
+        Pageable pageable = PaginationUtil.getPageable(request);
+        Page<DocumentEntity> entities = documentRepo.findAllByReviewer(reviewerId, pageable);
+        List<Document> content = entities.getContent().stream().map(mapper::toDomain).toList();
+
+        return new PagingResult<>(
+                content,
+                entities.getTotalPages(),
+                entities.getTotalElements(),
+                entities.getSize(),
+                entities.getNumber(),
+                entities.isLast(),
+                entities.hasNext()
+        );
     }
 
     @Override
     public PagingResult<Document> findAll(PaginationRequest request) {
-        return null;
+        Pageable pageable = PaginationUtil.getPageable(request);
+        Page<DocumentEntity> entities = documentRepo.findAll(pageable);
+        List<Document> content = entities.getContent().stream().map(mapper::toDomain).toList();
+
+        return new PagingResult<>(
+                content,
+                entities.getTotalPages(),
+                entities.getTotalElements(),
+                entities.getSize(),
+                entities.getNumber(),
+                entities.isLast(),
+                entities.hasNext()
+        );
     }
 
     @Override
     public List<DocumentStatusCount> countDocumentsByStatus() {
-        return List.of();
+        return documentRepo.countDocumentsByStatus();
     }
 
     @Override
     public Set<Document> findAllByIdIn(Set<UUID> ids) {
-        return Set.of();
+        return documentRepo.findAllByIdIn(ids).stream().map(mapper::toDomain).collect(Collectors.toSet());
     }
 }
