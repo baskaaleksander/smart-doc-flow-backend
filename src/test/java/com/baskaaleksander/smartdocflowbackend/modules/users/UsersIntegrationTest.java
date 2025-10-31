52641950743bfe0356f243059d1465fc19b0d2e6
Aleksander Baska <baskaaleksander03@gmail.com>
2025-10-31 15:55:21 +0100

    feat: add integration tests for user access control in UsersIntegrationTest

--- src/test/java/com/baskaaleksander/smartdocflowbackend/modules/users/UsersIntegrationTest.java
+++ src/test/java/com/baskaaleksander/smartdocflowbackend/modules/users/UsersIntegrationTest.java
@@ -1,13 +1,24 @@
 package com.baskaaleksander.smartdocflowbackend.modules.users;
 
+import com.baskaaleksander.smartdocflowbackend.modules.testsupport.AuthTestUtils;
 import com.baskaaleksander.smartdocflowbackend.modules.testsupport.IntegrationTestBase;
 import com.baskaaleksander.smartdocflowbackend.modules.testsupport.TestDataSeeder;
+import com.baskaaleksander.smartdocflowbackend.modules.testsupport.TestDataUtils;
 import com.baskaaleksander.smartdocflowbackend.modules.users.adapters.persistence.spring.SpringDataUserRepository;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 @AutoConfigureMockMvc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -22,8 +33,46 @@ public class UsersIntegrationTest extends IntegrationTestBase {
     @Autowired
     private SpringDataUserRepository userRepository;
 
+    @Autowired
+    private AuthTestUtils authUtils;
+
+    @Autowired
+    private TestDataUtils dataUtils;
+
     @BeforeAll
     void seed() {
         seeder.seedAccountsIfNotExists();
+    }
+
+    @Test
+    @DisplayName("ADMIN can get paginated list of all users")
+    void adminCanGetAllUsers() throws Exception {
+
+        String accessToken = authUtils.loginAndGetAccessToken("admin", "Admin#12345");
+
+        mockMvc.perform(get("/users/")
+                        .header("Authorization", "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isArray())
+                .andExpect(jsonPath("$.content.length()").value(3))
+                .andExpect(jsonPath("$.content[?(@.username=='admin')]").exists())
+                .andExpect(jsonPath("$.content[?(@.username=='user')]").exists())
+                .andExpect(jsonPath("$.totalElements").value(3))
+                .andExpect(jsonPath("$.page").value(0))
+                .andExpect(jsonPath("$.size").value(10))
+                .andExpect(jsonPath("$.last").value(true))
+                .andExpect(jsonPath("$.next").value(false));
+    }
+
+    @Test
+    @DisplayName("Normal USER cannot access list of all users")
+    void userCannotGetAllUsers() throws Exception {
+        String accessToken = authUtils.loginAndGetAccessToken("user", "User#12345");
+
+        mockMvc.perform(get("/users/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + accessToken)
+        ).andExpect(status().isForbidden());
     }
 }
