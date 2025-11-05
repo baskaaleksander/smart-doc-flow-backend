58ccd44f13381796726353d1aef583722796f8f8
Aleksander Baska <baskaaleksander03@gmail.com>
2025-11-05 18:39:58 +0100

    feat: add Redis cache configuration for improved caching support

---
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/common/config/RedisCacheConfig.java
@@ -0,0 +1,40 @@
+package com.baskaaleksander.smartdocflowbackend.common.config;
+
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration defaultConfig =
+                RedisCacheConfiguration.defaultCacheConfig()
+                        .entryTtl(Duration.ofMinutes(5))
+                        .disableCachingNullValues()
+                        .serializeValuesWith(
+                                RedisSerializationContext.SerializationPair
+                                        .fromSerializer(new GenericJackson2JsonRedisSerializer())
+                        );
+
+        Map<String, RedisCacheConfiguration> cacheConfigs = Map.of(
+                "auth:me", defaultConfig.entryTtl(Duration.ofMinutes(10))
+        );
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultConfig)
+                .withInitialCacheConfigurations(cacheConfigs)
+                .build();
+    }
+}
