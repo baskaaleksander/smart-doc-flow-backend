fff90064c8cf127aa2a44f07955dcade06885ed2
Aleksander Baska <baskaaleksander03@gmail.com>
2025-11-06 16:30:13 +0100

    feat: update application-test.yml and IntegrationTestBase for Redis and Swagger configuration

--- src/test/java/com/baskaaleksander/smartdocflowbackend/modules/testsupport/IntegrationTestBase.java
+++ src/test/java/com/baskaaleksander/smartdocflowbackend/modules/testsupport/IntegrationTestBase.java
@@ -1,6 +1,12 @@
 package com.baskaaleksander.smartdocflowbackend.modules.testsupport;
 
+import org.mockito.Mockito;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.qdrant.autoconfigure.QdrantVectorStoreAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -32,11 +38,17 @@ public abstract class IntegrationTestBase {
                     .withExposedPorts(9000, 9001)
                     .waitingFor(Wait.forHttp("/minio/health/ready").forPort(9000));
 
+    protected static final GenericContainer<?> REDIS =
+            new GenericContainer<>("redis:7-alpine")
+                    .withCommand("redis-server --requirepass password")
+                    .withExposedPorts(6379);
+
     static {
         POSTGRES.start();
         RABBIT.start();
         QDRANT.start();
         MINIO.start();
+        REDIS.start();
     }
 
     @DynamicPropertySource
@@ -50,10 +62,22 @@ public abstract class IntegrationTestBase {
         r.add("spring.rabbitmq.username", RABBIT::getAdminUsername);
         r.add("spring.rabbitmq.password", RABBIT::getAdminPassword);
 
+        r.add("spring.rabbitmq.stream.username", RABBIT::getAdminUsername);
+        r.add("spring.rabbitmq.stream.password", RABBIT::getAdminPassword);
+        r.add("spring.rabbitmq.stream.port", RABBIT::getAmqpPort);
+
         String minioUrl = "http://" + MINIO.getHost() + ":" + MINIO.getMappedPort(9000);
-        r.add("minio.url", () -> minioUrl);
+        r.add("minio.internalUrl", () -> minioUrl);
+        r.add("minio.externalUrl", () -> minioUrl);
         r.add("minio.access.name", () -> "minio");
         r.add("minio.access.secret", () -> "minio12345");
 
+        r.add("spring.ai.vectorstore.qdrant.host", QDRANT::getHost);
+        r.add("spring.ai.vectorstore.qdrant.port", () -> QDRANT.getMappedPort(6334));
+
+        r.add("spring.data.redis.host", REDIS::getHost);
+        r.add("spring.data.redis.port", () -> REDIS.getMappedPort(6379));
+        r.add("spring.data.redis.password", () -> "password");
     }
+    
 }
