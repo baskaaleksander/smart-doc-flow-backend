6f426542af5d493f8decff9477e9533eebbeb1e5
Aleksander Baska <baskaaleksander03@gmail.com>
2025-11-13 21:57:22 +0100

    feat: changed cache evict to cache put while editing account

--- src/main/java/com/baskaaleksander/smartdocflowbackend/modules/users/application/UserApplicationService.java
+++ src/main/java/com/baskaaleksander/smartdocflowbackend/modules/users/application/UserApplicationService.java
@@ -21,6 +21,7 @@ import com.baskaaleksander.smartdocflowbackend.modules.users.domain.view.UserSta
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -75,7 +76,7 @@ public class UserApplicationService {
     }
 
     @Transactional
-    @CacheEvict(key = "#userId")
+    @CachePut(cacheNames = "auth:me", key = "#userId")
     public AccountActionResponse inactivateUser(String userId) {
         UUID userUUID = UUID.fromString(userId);
 
@@ -118,7 +119,7 @@ public class UserApplicationService {
     }
 
     @Transactional
-    @CacheEvict(key = "#userId")
+    @CachePut(cacheNames = "auth:me", key = "#userId")
     public UserResponse editUserAccount(UUID userId, EditUserAccountAdminRequest editRequest) {
         logger.info("USR_EDIT_ADMIN START userId=" + Slf4jLoggingAdapter.shortId(userId)
                 + " emailHash=" + Slf4jLoggingAdapter.hashEmail(editRequest.email())
@@ -157,7 +158,7 @@ public class UserApplicationService {
     }
 
     @Transactional
-    @CacheEvict(key = "#userId")
+    @CachePut(cacheNames = "auth:me", key = "#userId")
     public UserResponse editSelfAccount(EditUserAccountRequest editRequest, UUID userId) {
         logger.info("USR_EDIT_SELF START userId=" + Slf4jLoggingAdapter.shortId(userId)
                 + " emailProvided=" + (editRequest.email() != null));
